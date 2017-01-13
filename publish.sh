0fd24059f0652a01f7cbe8b48cd06a8359569847
Bill Dwyer <bdwyer@palantir.com>
2017-01-13 12:56:49 -0800

    Use npmignore whitelist. Change d3 types versions. Remove es6-shim

--- publish.sh
+++ publish.sh
@@ -2,7 +2,21 @@
 
 set -e
 
+PACKAGE_NAME=$(npm run -s echo -- '$npm_package_name')
+CURRENT_VERSION=$(npm info $PACKAGE_NAME version)
+NEW_VERSION=$(npm run -s echo -- '$npm_package_version')
+
+if [ "$CURRENT_VERSION" == "$NEW_VERSION" ]; then
+  echo "Version number in package.json was not updated!"
+  exit 1
+else
+  echo "New version detected. Preparing $NEW_VERSION"
+fi
+
+# Prepare artifacts, one last check on tests
+npm test
+
+# Publish to npm
 # https://circleci.com/docs/npm-login/
 echo -e "$NPM_USER\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
-
 npm publish
